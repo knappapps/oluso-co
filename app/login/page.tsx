caee56c6ca7cea3abe3186a776cd5841cd57e103
knappapps <rknapp@gmail.com>
2026-06-24 14:22:55 -0600

    feat: add 'Forgot password?' link to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -67,7 +67,10 @@ function LoginForm() {
         </div>
 
         <div>
-          <label className="block text-xs font-medium text-gray-700 mb-1">Password</label>
+          <div className="flex items-center justify-between mb-1">
+            <label className="block text-xs font-medium text-gray-700">Password</label>
+            <Link href="/forgot-password" className="text-xs text-blue-600 hover:underline">Forgot password?</Link>
+          </div>
           <div className="relative">
             <Lock size={16} className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
             <input
